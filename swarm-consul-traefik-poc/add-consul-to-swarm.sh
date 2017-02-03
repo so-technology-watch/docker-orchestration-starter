4866686a6105bec79169679c51f4167e8a614ce4
Benoit Wilcox <wilcox.benoit@gmail.com>
2017-02-03 16:58:04 +0100

    Change network name

--- swarm-consul-traefik-poc/add-consul-to-swarm.sh
+++ swarm-consul-traefik-poc/add-consul-to-swarm.sh
@@ -10,19 +10,39 @@ else
     eval "$(docker-machine env leader1)"
 fi
 
-# Servers
+# Create Consul leader
 ###################################################################
-$passBySSHForWindows_leaderdocker service create --replicas 1 \
+echo 'Create Consul leader'
+$passBySSHForWindows_leader docker service create --replicas 1 \
     --name consul-leader \
     --publish 8500:8500 \
-    --network so-tech-net \
+    --network so_tech_net \
     --constraint=node.hostname=='leader1' \
     progrium/consul -server -bootstrap-expect 1 -ui-dir /ui
 
+until $passBySSHForWindows_leader docker ps --no-trunc -f name=consul-leader
+do
+    echo "wait for container"
+    sleep 1
+done
+
+# Create Consul Nodes
+###################################################################
+echo 'Create Consul Nodes, one per worker in the swarm'
+eval "$(docker-machine env leader1)"
+container_id=$($passBySSHForWindows_leader docker ps --no-trunc q -f name=consul-leader)
+echo " leader container-id: $container_id"
+
+docker-machine.exe ssh leader1 "docker inspect --format='{{.NetworkSettings.Networks.so_tech_net.IPAddress}}' $containerLeader"
+
+
+ip_consul_leader=$($passBySSHForWindows_leader docker network inspect so_tech_net --format )
+
+echo "consul-leader: $ip_consul_leader"
 
 $passBySSHForWindows_leader docker service create \
     --name consul-nodes \
     --publish 8501:8500 \
-    --network so-tech-net \
+    --network so_tech_net \
     --mode global \
-    progrium/consul -server -join 10.1.9.3 -ui-dir /ui
+    progrium/consul -server -join $ip_consul_leader -ui-dir /ui
